4cab0f1196f0e98cb71eed2c76c5e02b4ddd956c
Joao Marcelo <jmttozato@gmail.com>
2021-05-13 18:16:11 -0300

    add controller for users and listings

---
+++ api/controllers/listings.js
@@ -0,0 +1,101 @@
+const Listing = require("../models/listings");
+
+module.exports = {
+  get_all_listings: (req, res, next) => {
+    Listing.find()
+      .select("cryptoName price author _id")
+      .exec()
+      .then((result) => {
+        const message = {
+          count: result.length,
+          listings: result,
+        };
+
+        return res.status(200).json(message);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ error: err });
+      });
+  },
+  create_listing: (req, res, next) => {
+    const listing = new Listing({
+      _id: new mongoose.Types.ObjectId(),
+      cryptoName: req.body.cryptoName,
+      price: req.body.price,
+      author: req.body.author,
+    });
+
+    listing
+      .save()
+      .then((result) => {
+        console.log(result);
+        return res.status(200).json({
+          message: "Listing created sucessfully",
+          listing: result,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ error: err });
+      });
+  },
+  get_listing_by_id: (req, res, next) => {
+    const id = req.params.listingId;
+
+    Listing.findById(id)
+      .select("cryptoName price author _id")
+      .exec()
+      .then((result) => {
+        console.log(result);
+        if (result) {
+          return res.status(200).json(result);
+        } else {
+          return res
+            .status(404)
+            .json({ message: "No listing entry for the provided ID" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ error: err });
+      });
+  },
+  patch_listing: (req, res, next) => {
+    const id = req.params.listingId;
+
+    const updateOp = {};
+
+    for (const op of req.body) {
+      updateOp[op.propName] = op.value;
+    }
+
+    Listing.updateOne({ _id: id }, { $set: updateOp })
+      .exec()
+      .then((result) => {
+        console.log(result);
+        return res.status(200).json({
+          message: "Listing updated sucessfully",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ error: err });
+      });
+  },
+  delete_listing: (req, res, next) => {
+    const id = req.params.listingId;
+    Listing.remove({ _id: id })
+      .exec()
+      .then((result) => {
+        console.log(result);
+        return res.status(200).json({
+          message: "Listing deleted sucessfully",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ error: err });
+      });
+  },
+};
---
+++ api/controllers/user.js
@@ -0,0 +1,94 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  signup: (req, res, next) => {
+    User.find({ email: req.body.email })
+      .exec()
+      .then((result) => {
+        if (result.length > 0) {
+          return res.status(409).json({
+            message: "Email already registered",
+          });
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              return res.status(500).json({
+                error: err,
+              });
+            } else {
+              const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash,
+              });
+              user
+                .save()
+                .then((result) => {
+                  return res.status(201).json({
+                    message: "User created sucessfully",
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                  res.status(500).json({
+                    error: err,
+                  });
+                });
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  },
+  login: (req, res, next) => {
+    User.findOne({ email: req.body.email })
+      .exec()
+      .then((result) => {
+        if (result.length < 1) {
+          return res.status(401).json({
+            message: "Authentication failed",
+          });
+        }
+
+        bcrypt.compare(req.body.password, result.password, (err, response) => {
+          if (err) {
+            return res.status(401).json({
+              message: "Authentication failed",
+            });
+          }
+          if (response) {
+            const token = jwt.sign(
+              {
+                email: result.email,
+                userId: result._id,
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: "1h",
+              }
+            );
+            return res.status(200).json({
+              message: "Auth sucessfull",
+              token: token,
+            });
+          }
+          return res.status(401).json({
+            message: "Authentication failed",
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  },
+};
