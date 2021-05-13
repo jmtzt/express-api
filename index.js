785422f70004f088da116cb547b3d4d6e1d79007
Joao Marcelo <jmttozato@gmail.com>
2021-05-13 14:57:11 -0300

    add route error handling

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const port = process.env.PORT || 3000;
 
 const listingRoutes = require("./api/routes/listings");
 
+app.use(morgan("dev"));
+
 app.use("/listings", listingRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// error handling route
+app.use((req, res, next) => {
+  const error = new Error("Route not found!");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 app.listen(port, () => {
